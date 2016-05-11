d2fe03b20cfd77b567a5f4bc0fabbceba7097bcd
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-05-11 18:35:56 +0300

    animated buttons

--- app/views/catalog/catalog.js
+++ app/views/catalog/catalog.js
@@ -1,51 +1,83 @@
 "use strict";
 var frameModule = require("ui/frame");
+var stack;
+var allButton;
+var silverButton;
+var goldButton;
+var copperButton;
+function onLoaded(args) {
+    stack = args.object;
+    silverButton = stack.getViewById("btn-silver");
+    goldButton = stack.getViewById("btn-gold");
+    allButton = stack.getViewById("btn-all");
+    copperButton = stack.getViewById("btn-copper");
+}
+exports.onLoaded = onLoaded;
 function goToCopper(args) {
-    console.log("IN COOPER");
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/copper",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
+    copperButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/copper",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
     });
 }
 exports.goToCopper = goToCopper;
 function goToSilver(args) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/silver",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
+    silverButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/silver",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
     });
 }
 exports.goToSilver = goToSilver;
 function goToGold(args) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/gold",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
+    goldButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/gold",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
     });
 }
 exports.goToGold = goToGold;
 function goToAll(args) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/all-coins",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
+    allButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/all-coins",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
     });
 }
 exports.goToAll = goToAll;
--- app/views/catalog/catalog.ts
+++ app/views/catalog/catalog.ts
@@ -1,52 +1,89 @@
 import { EventData } from "data/observable";
 import frameModule = require("ui/frame");
+import { StackLayout } from "ui/layouts/stack-layout";
+
+var stack;
+
+var allButton;
+var silverButton; 
+var goldButton;
+var copperButton;
+
+export function onLoaded(args:EventData) {
+    stack = <StackLayout>args.object;
+    
+    silverButton = stack.getViewById("btn-silver");
+    goldButton = stack.getViewById("btn-gold");
+    allButton = stack.getViewById("btn-all");
+    copperButton = stack.getViewById("btn-copper");
+}
 
 export function goToCopper(args:EventData) {
-    console.log("IN COOPER")
     
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/copper",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
-    });
+    copperButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {    
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/copper",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
+    })
+
 }
 
 export function goToSilver(args:EventData) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/silver",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
-    });
+    silverButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {    
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/silver",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
+    })
 }
 
 export function goToGold(args:EventData) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/gold",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
-    });
+    goldButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {    
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/gold",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
+    }) 
 }
 
 export function goToAll(args:EventData) {
-    frameModule.topmost().navigate({
-        moduleName: "views/categories/all-coins",
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 300,
-            curve: "easeIn"
-        }
-    });
+    allButton.animate({
+        rotate: 360,
+        duration: 500
+    }).then(function () {    
+        frameModule.topmost().navigate({
+            moduleName: "views/categories/all-coins",
+            animated: true,
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "easeIn"
+            }
+        });
+    })
 }
